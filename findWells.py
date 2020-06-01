0f49c302d19219e7861792284bf8147dcd804bac
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-06-01 16:40:22 -0500

    findWells
    Find wells now works as expected due to assistance from Professor Barnes

--- findWells.py
+++ findWells.py
@@ -9,55 +9,65 @@ PreRequisites:
 -------------
 Author: Jonny Full
 
-Version: 5/20/2020
+Version: 5/29/2020
 """
 
 import arcpy
 import numpy as np
-import sys
-import time
-from DataLocation import allwells, CWIPL, CWIST
+from DataLocation import allwells
 from scipy import spatial
+import time
 #from Verify import strID
 
-def findWells():
-    strID = str(593596) #remove        
-    with arcpy.da.SearchCursor(CWIST , ["WELLID"], "WELLID = " + strID) as cursor:
-        for row in cursor:   
-            break
-        else:
-            print("Well ID not found.")
-            sys.exit()  #Terminates the function
+def findWells(strID, radius):
+    start_time = time.time()
             
-    # Process: Join Field (Join Field) 
-    allwells_2_ = arcpy.AddJoin_management(allwells, "RELATEID", CWIPL, "RELATEID", "KEEP_COMMON")[0]
-    print("Well 2 done")
-    
-    # Process: Join Field (2) (Join Field) 
-    allwells_3_ = arcpy.AddJoin_management(allwells_2_, "RELATEID",CWIST, "RELATEID", "KEEP_COMMON")[0]
-    print("Well 3 Done")
-
+    targetWell = []        
+    with arcpy.da.SearchCursor(allwells, ['SHAPE', 'AQUIFER'], f"RELATEID = '{strID}'") as cursor: 
+        
+        for row in cursor:
+            targetWell.append(row) #redefine targetwell
+        targetWell = targetWell[0]
+    print(f'Search 1 Complete {time.time() - start_time}')        
+    #with arcpy.da.SearchCursor(allwells_2_, ["allwells.SHAPE", "allwells.AQUIFER", "C5PL.WELLID"], f"allwells.AQUIFER = '{AQ}'") as cursor:   
     well_data = []
-    
-    well_data = arcpy.da.SearchCursor(allwells_3_, ["allwells.SHAPE","C5ST.WELLID"], "WELLID = " + strID)
-    
-    
-    
+    with arcpy.da.SearchCursor(allwells, ["SHAPE", "AQUIFER", "RELATEID"], f"AQUIFER = '{targetWell[1]}'") as cursor:         
+        for row in cursor:
+            well_data.append(row)
+
+    print(f'Search 2 Complete {time.time() - start_time} {len(well_data)}')    
     xy = np.array([[well[0][0], well[0][1]] for well in well_data])
-    tree = spatial.cKDTree(xy[:, 0:1])
-    #xtarget, ytarget =  arcpy.da.SearchCursor(allwells_3_, ["UTME", "UTMN", "C5ST.WELLID"], "WELLID = " + ID)
+    
+    tree = spatial.cKDTree(xy)
+    
+    selectedWellindex = tree.query_ball_point(targetWell[0], radius) #I am stumped ask Barnes 
+    selectedWells = []
+    for i in selectedWellindex:
+        selectedWells.append(well_data[i])
+    
+    print(f'Total Time {time.time() - start_time} {len(selectedWells)}')    
+    return selectedWells
+            
+
+
+
+   
+    
+#selectedWells = findWells(strID, radius)
+
+#Remove Aquifer()
+
+def Aquifer():
+    with arcpy.da.SearchCursor(allwells_3_, ["allwells.SHAPE", "AQUIFER", "C5PL.WELLID"], "WELLID = " + strID) as cursor:
+    
+        for row in cursor:
+            if row[1] == AQ:
+                well_data.append(row)
     
     
-    query = tree.query_ball_point(xy, 5)
-    print(query)
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
