c04b93e17fed8a1a88d82637214272c9ae528a7a
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-06-11 14:10:19 -0500

    Update Transmissivity
    Made a small revision to Transmissivity function. Now correctly accounts for partial penetration factor.

--- Transmissivity.py
+++ Transmissivity.py
@@ -13,20 +13,17 @@ INPUTS:
     b = Aquifer Thickness (ft)
     rw = radius of influence (ft)
     Co = Step-Drawdown Test
-    
+
 OUTPUTS:
-    
-    Transmissivity (ft^2/day)
-    
-    
+Transmissivity (ft^2/day)
+
 ASSUMPTIONS:
-    Radius of Influence = Well Radius
-    Aquifer is infidently long
-    Constant Pump Rate
-    Homogonous Nonleaky Aquifer
-    
-@author: Jonny Full
-Created on Mon May 18 09:01:15 2020
+Radius of Influence = Well Radius
+Aquifer is infidently long
+Constant Pump Rate
+Homogonous Nonleaky Aquifer
+Author: Jonny Full
+Version: 6/11/2020
 -------------------------------------------------------------------------------
 """
 """
@@ -38,9 +35,6 @@ make things simpiler.
 import math
 from pump_log_calc import pump_log
 from allwells_calc import allwells_data
-
-
-
 
 def calc(relate_id):
     Q = [] #gpm
@@ -54,25 +48,22 @@ def calc(relate_id):
     #sw = Co * Q**2 
     T = 1
     sp = 0
-    S = 0.001 #storativity = S temporary constant     
-
+    S = 0.001 #storativity = S temporary constant
     Q = [float()] #gpm
     s = [float()] #ft
     t = [float()] #day
     rw = [float()] #ft
     L = [float()] #ft
     VALUE = []
-    
     Q, t, s = pump_log(relate_id)
 
-    
     L, rw = allwells_data(relate_id)
-    
+
     for i in range(len(Q)):
         T = 1.0
         LastValue = 0
         Lb = L[i]/b  
-        G =  2.948 - (7.363*(Lb)) + (11.447*((Lb)**2)) - (4.675*((Lb)**3))
+        G = 2.948 - (7.363*(Lb)) + (11.447*((Lb)**2)) - (4.675*((Lb)**3))
         if L[i] > 0 and rw[i] > 0:
             sp = ((1-Lb)/Lb)*(math.log(b/rw[i])-G)
         else:
@@ -80,19 +71,10 @@ def calc(relate_id):
         while (T - LastValue) >= 0.001:
             LastValue = T
             if Q[i] > 0 and t[i] > 0 and L[i] > 0 and rw[i] > 0 and s[i] > 0:
-                T = (Q[i] /(4*math.pi*(s[i])))*(math.log(2.25*T* t[i] /((rw[i] **2) * S))) + (2*sp)
+                T = (Q[i] /(4*math.pi*(s[i])))*(math.log(2.25*T* t[i] /((rw[i] **2) * S)) + (2*sp))
             else:
                 T = 0
 
         VALUE.append(T)
 
     return VALUE
-    
-
-
-
-
-
-
-
-
