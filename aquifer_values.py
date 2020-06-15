503a7b6f4a7c60bd830c888e11e373ea821b3965
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-06-15 16:37:20 -0500

    Aquifer Value Update
    Updated my aquifer value code so it is more in line with Pep 8 standards.

---
+++ aquifer_values.py
@@ -0,0 +1,41 @@
+"""
+This is a helper function for Transmissivity that retrieves the stroativity
+of aquifers from Justin Blum's pump test spreadsheet. This spreadsheet is then
+read and dumped into a .JSON file. This function only needs to be executed once
+for the values to be organized.
+
+Notes:
+    This function requires Data Location and Verify to run properly.
+
+Author: Jonny Full
+Version: 6/15/2020
+"""
+import json
+import arcpy
+from DataLocation import loc
+#from findWells import selectedWells
+def store_sheet():
+    input_excel = loc
+    sheet_name = "data"
+    memory_table = "in_memory" + "\\" + "memoryTable"
+    #Make sure the memory is empty
+    arcpy.Delete_management(memory_table)
+    arcpy.ExcelToTable_conversion(input_excel, memory_table, sheet_name)
+    storativity = {}
+    field_indices = {}
+    fields = ['Relate_ID', 'Storativity']
+    for i, field in enumerate(fields):
+        storativity[field] = []
+        field_indices[i] = field
+    with arcpy.da.SearchCursor(memory_table, ['Relateid', 'testS']) as cursor:
+        for row in cursor:
+            for i in range(len(row)):
+                if row[1] != 0 and row[1] is not None:
+                    storativity[field_indices[i]].append(row[i])
+    store_data = json.dumps(storativity)
+    outfile = open('storativity_data.json', 'w')
+    outfile.write(store_data)
+    return storativity
+
+store_sheet()
+ 
