0c6f5de85648bfbddec003da682688f181fc09ec
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-06-16 10:34:45 -0500

    getWells
    Uploaded updated getWells file. Removed timing code.

--- findWells.py
+++ findWells.py
@@ -9,18 +9,18 @@ PreRequisites:
 -------------
 Author: Jonny Full
 
-Version: 5/29/2020
+Version: 6/16/2020
 """
 
 import arcpy
 import numpy as np
 from DataLocation import allwells
 from scipy import spatial
-import time
+
 
 
 def findWells(ID, RADIUS):
-    start_time = time.time()
+
             
     targetWell = []        
     with arcpy.da.SearchCursor(allwells, ['SHAPE', 'AQUIFER'], f"RELATEID = '{ID}'") as cursor: 
@@ -28,14 +28,14 @@ def findWells(ID, RADIUS):
         for row in cursor:
             targetWell.append(row) #redefine targetwell
         targetWell = targetWell[0]
-    print(f'Search 1 Complete {time.time() - start_time}')        
+       
     #with arcpy.da.SearchCursor(allwells_2_, ["allwells.SHAPE", "allwells.AQUIFER", "C5PL.WELLID"], f"allwells.AQUIFER = '{AQ}'") as cursor:   
     well_data = []
     with arcpy.da.SearchCursor(allwells, ["SHAPE", "AQUIFER", "RELATEID"], f"AQUIFER = '{targetWell[1]}'") as cursor:         
         for row in cursor:
             well_data.append(row)
 
-    print(f'Search 2 Complete {time.time() - start_time} {len(well_data)}')    
+        
     xy = np.array([[well[0][0], well[0][1]] for well in well_data])
     
     tree = spatial.cKDTree(xy)
@@ -45,7 +45,7 @@ def findWells(ID, RADIUS):
     for i in selectedWellindex:
         selectedWells.append(well_data[i])
     
-    print(f'Total Time {time.time() - start_time} {len(selectedWells)}')    
+       
     return selectedWells
             
 
@@ -53,9 +53,7 @@ def findWells(ID, RADIUS):
 
    
     
-#selectedWells = findWells(strID, radius)
 
-#Remove Aquifer()
 
 
     
