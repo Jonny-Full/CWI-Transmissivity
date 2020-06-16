7af3a0193723f46ce064dd192ef240d24d14adbf
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-06-16 15:16:06 -0500

    analyize_wells Update
    Forgot to upload this with the previous commit

--- analyze_wells.py
+++ analyze_wells.py
@@ -8,18 +8,21 @@ Notes:
     -This file requires Data Location, findWells, Transmissivity, and Verify to execute
 
 @author: Jonny Full
-Version: 6/15/2020
+Version: 6/16/2020
 -------------------------------------------------------------------------------
 """
 #COMPLETE
 from Verify import Verify
 from findWells import findWells
 from Transmissivity import calc, Conduct
+from plots import plot_T
+
 ID, rad = Verify()
 RADIUS = int(rad) #meters
 selected_wells = findWells(ID, RADIUS)
+location = [i[0] for i in selected_wells]
 aqui_id = [i[1] for i in selected_wells] #incorperate into Storativity/ Thickness calculations
 relate_id = [i[2] for i in selected_wells]
 TSIV = calc(relate_id)
 HYDCONDUCT = Conduct(TSIV)
-
+plot_T(TSIV)
