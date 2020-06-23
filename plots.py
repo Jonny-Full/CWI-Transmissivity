b7b4ad6b45968cfdc62377c272df859ef71633d8
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-06-23 17:14:27 -0500

    Plots Commit
    Updated ploting function. The data can now be viewed spacially

--- plots.py
+++ plots.py
@@ -10,16 +10,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_T(TSIV):
-     plt.hist(np.log(TSIV), bins=50)
-     plt.title('Transmissivity Distribution')
-     plt.xlabel('ln(T)')
-     plt.ylabel('Number of entries')
+    plt.hist(np.log(TSIV), bins=50)
+    plt.title('Transmissivity Distribution')
+    plt.xlabel('ln(T)')
+    plt.ylabel('Number of entries')
      
-def spacial_T(TSIV, location):
+def spacial_T(confirmed_wells, TSIV):
     #This does not work due to indexing
-    x = [i[0] for i in location]
-    y = [i[1] for i in location]
-    
+    x = [i[0][0] for i in confirmed_wells]
+    y = [i[0][1] for i in confirmed_wells]
     plt.grid(True)
     plt.scatter(x, y, 12, TSIV)
     plt.colorbar()
