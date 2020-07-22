5f387c0ec8aa60e9eec1dba81718b64deab6d66e
Jonny-Full <65545581+Jonny-Full@users.noreply.github.com>
2020-07-22 16:18:10 -0500

    Added Hydralic Conductivity Plot
    I added a function that plots the hydralic conductivity. I also added code so the target well now appears as a black square.

--- analyze_wells.py
+++ analyze_wells.py
@@ -25,16 +25,26 @@ from Verify import Verify
 from Transmissivity import transmissivity_calculations, conductivity_calculations
 from data_retrieve import find_wells, data_organization, pump_log,\
 aquifer_thickness, storativity_calculations
-from plots import plot_histogram_transmissivity, plot_spacial_transmissivity
-
+from plots import plot_histogram_transmissivity, plot_spacial_transmissivity, plot_spacial_conductivity
+target_coords = []
 target_well, rad = Verify()
 radius = int(rad) #meters
 candidate_wells = find_wells(target_well, radius)
+
+for row in candidate_wells:
+    if int(target_well) == row[5]:
+        utm_e = row[0]
+        utm_n = row[1]
+        well_id = row[5]
+        data = [utm_e, utm_n, well_id]
+        target_coords.append(data)
+        
 pump_log_results = pump_log(candidate_wells)
 thickness_data = aquifer_thickness(candidate_wells)
 thickness_storativity_data = storativity_calculations(candidate_wells, thickness_data)
 confirmed_wells = data_organization(candidate_wells, pump_log_results, thickness_storativity_data)
 transmissivity_calculated = transmissivity_calculations(confirmed_wells)
-HYDCONDUCT = conductivity_calculations(confirmed_wells, transmissivity_calculated)
+conductivity_calculated = conductivity_calculations(confirmed_wells, transmissivity_calculated)
 plot_histogram_transmissivity(transmissivity_calculated)
-plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissivity_calculated)
+plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissivity_calculated, target_coords)
+plot_spacial_conductivity(target_well, radius, confirmed_wells, conductivity_calculated, target_coords)
--- plots.py
+++ plots.py
@@ -49,7 +49,7 @@ def plot_histogram_transmissivity(transmissivity_calculated):
     plt.legend(loc = 'upper right')
 
     
-def plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissivity_calculated):
+def plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissivity_calculated, target_coords):
     """Plots the confirmed_wells geographical location and shows the 
     Transmissivity for each well.
     
@@ -79,6 +79,11 @@ def plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissi
     transmissivity_calculated: list
         A list of Transmissivities that were calculated from confirmed_wells. 
         The units are ft^2/day.
+    
+    target_coords:list
+        Contains the UTM coordinates of the original well input by the user.
+        This data will be used to create a unique data point on the scatterplot
+        to represent the location of the target well.
         
     Notes
     -----
@@ -90,7 +95,7 @@ def plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissi
     distribute_t = []
     x = [i[0][0] for i in confirmed_wells]
     y = [i[0][1] for i in confirmed_wells]
-    T = [i for i in transmissivity_calculated]
+    T = [i[0] for i in transmissivity_calculated]
     bounds = np.percentile(transmissivity_calculated, np.arange(0, 100, 10)) #calculates deciles
     for row in T:
         value = 0
@@ -115,9 +120,97 @@ def plot_spacial_transmissivity(target_well, radius, confirmed_wells, transmissi
         else:
             value = 10
         distribute_t.append(value)
-    sns.cubehelix_palette(dark=.3, light=.8, as_cmap=True)
-    sns.scatterplot(x, y, hue = distribute_t, palette = "Set2") #Set2 allows for the gradient
+    sns.cubehelix_palette(n_colors = 10, as_cmap=True)
+    sns.scatterplot(x, y, hue = distribute_t, palette = 'Set2', s = 40) #Set2 allows for the gradient
+    sns.scatterplot([target_coords[0][0]], [target_coords[0][1]], color='black', marker = 's', s = 50)
+    plt.legend(labels = ['0-10', '10-20', '20-30', '30-40', '40-50',\
+                         '50-60', '60-70', '70-80', '80-90', '90-100'],\
+                title = "Deciles for Transmissivity (%)")
     plt.title(f"Transmissivity for Wells within {radius} meters of Well ID {target_well}")
+    plt.xlabel("UTM Easting")
+    plt.ylabel("UTM Northing")
+    plt.axis('equal')
+    plt.grid(True)
+    
+def plot_spacial_conductivity(target_well, radius, confirmed_wells, conductivity_calculated, target_coords):
+    """Plots the confirmed_wells geographical location and shows the 
+    Transmissivity for each well.
+    
+    Parameters
+    ----------
+    ----------
+    target_well: string
+        The RELATEID of the well input by the user in Verify.
+        Example : '0000123456'
+        
+    RADIUS: int (meters)
+        RADIUS represents a boundry condition for the scope of analysis. 
+        Any wells used in future calculations fall within this distance of the
+        target well. 
+        Example: 10000 (meters)
+        
+    confirmed_wells: list[[list1][list2]]
+        This is a list of all pertinant information required to plot the wells
+        spacially and calculate Transmissivity.
+    
+        list1 = list[UTME (int), UTMN (int), AQUIFER (str), Screen Length (float), 
+                     Casing Radius (ft), Relate ID (str)
+        
+        list 2 = list[Pump Rate (float), Duration (float), Drawdown (float), 
+                     Relate ID (str)]
+    
+    conductivity_calculated: list
+        A list of hydralic conductivities that were calculated from confirmed_wells. 
+        The units are ft/day.
+    
+    target_coords:list
+        Contains the UTM coordinates of the original well input by the user.
+        This data will be used to create a unique data point on the scatterplot
+        to represent the location of the target well.
+        
+    Notes
+    -----
+    This function returnes a scatter plot. The axis will use the UTM Easting (x)
+    and UTM Northing (y) on its axis. The plotted points will be color coded
+    depending on the decile that their respective transmissivity fall into.
+    """
+    
+    plt.figure(3)
+    distribute_K = []
+    x = [i[0][0] for i in confirmed_wells]
+    y = [i[0][1] for i in confirmed_wells]
+    T = [i[0] for i in conductivity_calculated]
+    bounds = np.percentile(conductivity_calculated, np.arange(0, 100, 10)) #calculates deciles
+    for row in T:
+        value = 0
+        if row < bounds[1]:
+            value = 1
+        elif row < bounds[2]:
+            value = 2
+        elif row < bounds[3]:
+            value = 3
+        elif row < bounds[4]:
+            value = 4
+        elif row < bounds[5]:
+            value = 5
+        elif row < bounds[6]:
+            value = 6
+        elif row < bounds[7]:
+            value = 7
+        elif row < bounds[8]:
+            value = 8
+        elif row < bounds[9]:
+            value = 9
+        else:
+            value = 10
+        distribute_K.append(value)
+    sns.cubehelix_palette(n_colors = 10, as_cmap=True)
+    sns.scatterplot(x, y, hue = distribute_t, palette = 'Set2', s = 40) #Set2 allows for the gradient
+    sns.scatterplot([target_coords[0][0]], [target_coords[0][1]], color='black', marker = 's', s = 50)
+    plt.legend(labels = ['0-10', '10-20', '20-30', '30-40', '40-50',\
+                         '50-60', '60-70', '70-80', '80-90', '90-100'],\
+                title = "Deciles for Hydralic Conductivity (%)")
+    plt.title(f"Hydralic Conductivities for Wells within {radius} meters of Well ID {target_well}")
     plt.xlabel("UTM Easting")
     plt.ylabel("UTM Northing")
     plt.axis('equal')
@@ -146,36 +239,18 @@ def Pump_Durations_Plots():
            
         DUR_DATA = [i[1] for i in VALUES]
         PUMP_DATA = [i[0] for i in VALUES]
-        plt.figure(1)
-        plt.hist(DUR_DATA, bins = 100)
-        plt.title('# of Durations')
-        plt.xlabel('DURATION')
-        plt.ylabel('Number of Entries')
-        plt.xticks(np.arange(0, 13, step = 1))
-        plt.yticks(np.arange(0, 130000, step = 10000))
-       
-        
-        
-        plt.figure(2)
-        plt.hist(PUMP_DATA, bins = 100)
-        plt.title('# of Pump Rates')
-        plt.xlabel('Pump Rate')
-        plt.ylabel('Number of Entries')
-        plt.xticks(np.arange(0, 110, step = 10))
-        plt.yticks(np.arange(0, 60000, step = 5000))
-        plt.minorticks_on()
-       
-        
+
         with PdfPages('DATA.pdf') as pdf:        
             fig, ax = plt.subplots(1,1)
-            ax.set_axisbelow(True)
-            plt.figure(3)
+            plt.figure(2)
             plt.scatter(PUMP_DATA, DUR_DATA)
+            plt.xticks(fontsize = 14)
+            plt.yticks(fontsize = 14)
             plt.minorticks_on()
             plt.xlim([0,100])
             plt.ylim([0,12])
-            plt.xlabel('Pumping Rates [GPM]', fontsize = 12)
-            plt.ylabel('Duration of Test [hours]', fontsize = 12)
+            plt.xlabel('Pumping Rates [GPM]', fontsize = 16)
+            plt.ylabel('Duration of Test [hours]', fontsize = 16)
             ax.xaxis.set_major_locator(MultipleLocator(20))
             ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
             ax.xaxis.set_minor_locator(MultipleLocator(10))
@@ -184,6 +259,7 @@ def Pump_Durations_Plots():
             ax.yaxis.set_minor_locator(MultipleLocator(0.5))
             plt.grid(which='minor', linestyle=':', linewidth='0.5', color='gray', zorder = 0)
             plt.grid(which='major', linestyle='-', linewidth='0.5', color='black', zorder = 0)
+            ax.set_axisbelow(True)
             pdf.savefig()
         
          
